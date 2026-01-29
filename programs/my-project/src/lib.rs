b9c2c7af4648bc58050e493890a68d58e57c7055
darkodi <darko.deuric@ethernal.tech>
2026-01-29 19:23:37 +0100

    add basic accounts, initialize and set

--- programs/my-project/src/lib.rs
+++ programs/my-project/src/lib.rs
@@ -3,14 +3,42 @@ use anchor_lang::prelude::*;
 declare_id!("HUBqtg5NkMCyygzyxx9Vc4X389icp6i9pXPV9pnrkEaH");
 
 #[program]
-pub mod my_project {
+pub mod basic_storage {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn initialize(_ctx: Context<Initialize>) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn set(ctx: Context<Set>, new_x: u64) -> Result<()> {
+        ctx.accounts.my_storage.x = new_x;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Set<'info> {
+    #[account(mut, seeds = [], bump)]
+    pub my_storage: Account<'info, MyStorage>,
+}
+
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    #[account(init,
+              payer = signer,
+              space= MyStorage::INIT_SPACE + 8,
+              seeds = [],
+              bump)]
+    pub my_storage: Account<'info, MyStorage>,
+
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct MyStorage {
+    x: u64,
+}
