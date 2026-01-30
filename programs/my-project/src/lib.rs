a809d4bb4b101320f01763dd07c0c3c6e2329cb3
darkodi <darko.deuric@ethernal.tech>
2026-01-30 15:06:27 +0100

    init if needed

--- programs/my-project/src/lib.rs
+++ programs/my-project/src/lib.rs
@@ -1,67 +1,36 @@
 use anchor_lang::prelude::*;
-use anchor_lang::system_program;
 use std::mem::size_of;
-use std::str::FromStr;
 
 declare_id!("HUBqtg5NkMCyygzyxx9Vc4X389icp6i9pXPV9pnrkEaH");
 
 #[program]
-pub mod crowdfund {
+pub mod init_if_needed {
     use super::*;
 
-    pub fn initialize(_ctx: Context<Initialize>) -> Result<()> {
-        Ok(())
-    }
-
-    pub fn donate(ctx: Context<Donate>, amount: u64) -> Result<()> {
-        let cpi_context = CpiContext::new(
-            ctx.accounts.system_program.to_account_info(),
-            system_program::Transfer {
-                from: ctx.accounts.signer.to_account_info().clone(),
-                to: ctx.accounts.pda.to_account_info().clone(),
-            },
-        );
-
-        system_program::transfer(cpi_context, amount)?;
-
-        Ok(())
-    }
-
-    pub fn withdraw(ctx: Context<Withdraw>, amount: u64) -> Result<()> {
-        ctx.accounts.pda.sub_lamports(amount)?;
-        ctx.accounts.signer.add_lamports(amount)?;
+    pub fn increment(ctx: Context<Initialize>) -> Result<()> {
+        let current_counter = ctx.accounts.my_pda.counter;
+        ctx.accounts.my_pda.counter = current_counter + 1;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
 pub struct Initialize<'info> {
+    #[account(
+        init_if_needed,
+        payer = signer,
+        space = size_of::<MyPDA>() + 8,
+        seeds = [],
+        bump
+    )]
+    pub my_pda: Account<'info, MyPDA>,
+
     #[account(mut)]
     pub signer: Signer<'info>,
-
-    #[account(init, payer = signer, space=size_of::<Pda>() + 8, seeds=[], bump)]
-    pub pda: Account<'info, Pda>,
     pub system_program: Program<'info, System>,
-}
-
-#[derive(Accounts)]
-pub struct Donate<'info> {
-    #[account(mut)]
-    pub signer: Signer<'info>,
-
-    #[account(mut)]
-    pub pda: Account<'info, Pda>,
-    pub system_program: Program<'info, System>,
-}
-
-#[derive(Accounts)]
-pub struct Withdraw<'info> {
-    #[account(mut, address = Pubkey::from_str("7MmcimWgSKRjmyBjGsGXVRkaHyMZ5g3kaFqH2QzmUhGt").unwrap())]
-    pub signer: Signer<'info>,
-
-    #[account(mut)]
-    pub pda: Account<'info, Pda>,
 }
 
 #[account]
-pub struct Pda {}
+pub struct MyPDA {
+    pub counter: u64,
+}
--- tests/my-project.ts
+++ tests/my-project.ts
@@ -1,54 +1,20 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
-import { Crowdfund } from "../target/types/crowdfund";
+import { InitIfNeeded } from "../target/types/init_if_needed";
 
-describe("crowdfund", () => {
+describe("init_if_needed", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
-  const program = anchor.workspace.Crowdfund as Program<Crowdfund>;
+  const program = anchor.workspace.InitIfNeeded as Program<InitIfNeeded>;
 
   it("Is initialized!", async () => {
-    const programId = await program.account.pda.programId;
+    const [myPda, _bump] = anchor.web3.PublicKey.findProgramAddressSync([], program.programId);
+    await program.methods.increment().accountsPartial({myPda: myPda}).rpc();
+    await program.methods.increment().accountsPartial({myPda: myPda}).rpc();
+    await program.methods.increment().accountsPartial({myPda: myPda}).rpc();
 
-    let seeds = [];
-    let pdaAccount = anchor.web3.PublicKey.findProgramAddressSync(seeds, programId)[0];
-
-    await program.methods.initialize().accountsPartial({
-      pda: pdaAccount
-    }).rpc();
-    
-    console.log("lamport balance of pdaAccount",
-    await anchor.getProvider().connection.getBalance(pdaAccount));
-    // transfer 2 SOL
-    await program.methods.donate(new anchor.BN(2_000_000_000)).accounts({
-      pda: pdaAccount
-    }).rpc();
-
-    console.log("lamport balance of pdaAccount after donation",
-    await anchor.getProvider().connection.getBalance(pdaAccount));
-
-    // transfer back 1 SOL
-    // the signer is the permitted address
-    await program.methods.withdraw(new anchor.BN(1_000_000_000)).accounts({
-      pda: pdaAccount
-    }).rpc();
-
-
-    console.log("lamport balance of pdaAccount after withdrawal",
-    await anchor.getProvider().connection.getBalance(pdaAccount));  
-    // try to transfer back another 1_000_000_001 lamports, rent exemption should prevent this
-    try {
-      await program.methods.withdraw(new anchor.BN(1_000_000_001)).accounts({
-        pda: pdaAccount
-      }).rpc();
-    } catch (e) {
-      console.log("expected error on over-withdrawal:", e);
-    }
-
-    // check balance again to confirm no change
-    console.log("lamport balance of pdaAccount after failed withdrawal",
-    await anchor.getProvider().connection.getBalance(pdaAccount));
-
+    let result = await program.account.myPda.fetch(myPda);
+    console.log(`counter is ${result.counter}`);
   });
 });
